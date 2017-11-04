c9fee714b63903f183382528e120197a117a4165
Grayson <a7mdbh98@gmail.com>
2017-11-04 13:22:22 +0300

    just trying out more things i guess

---
+++ src/cp2project/CP2Project.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cp2project;
+
+/**
+ *
+ * @author moon pc
+ */
+public class CP2Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
